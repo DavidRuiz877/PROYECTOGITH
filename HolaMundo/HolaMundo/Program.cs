6765a950d83b05975a2b708d638ce20ae938c76b
David_Ruiz <daruiz55@misena.edu.co>
2020-04-21 19:18:34 -0500

    Inicializando Repositorio remoto

---
+++ HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hola Mundo!");
+        Console.WriteLine("Este Es Mi Primer Programa En C#!");
+        Console.ReadLine();
+    }
+}
+
